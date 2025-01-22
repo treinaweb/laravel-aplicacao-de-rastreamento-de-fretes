ac602e73ef3cf25604f3b2be23a1ea60f95b4838
Elton Fonseca <elton869@gmail.com>
2025-01-22 18:10:17 -0300

    15 - Evitando erro quando o código de rastreamento não existe

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -21,6 +21,15 @@
             </div>
         </nav>
 
+        @if (session('error'))
+            <div class="container mx-auto px-4 pt-20">
+                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                    <strong class="font-bold">Erro!</strong>
+                    <span class="block sm:inline">{{ session('error') }}</span>
+                </div>
+            </div> 
+        @endif
+
         <div class="container mx-auto px-4 pt-16">
             {{ $slot }}
         </div>
