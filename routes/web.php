c1c5378fc817c4c02eb476d376968f43aa821d59
Elton Fonseca <elton869@gmail.com>
2025-01-22 14:45:20 -0300

    09 - Criando a página de rastreamento

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RastreamentoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class);
+Route::get('/rastreamento', RastreamentoController::class);
